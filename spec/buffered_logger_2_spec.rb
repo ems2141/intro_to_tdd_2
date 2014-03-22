0a0b2bcf0ae8d9b43927d23fdaa8059646146f2a
Ellie Schneiders <ems2141@gmail.com>
2014-03-22 11:41:39 -0600

    Logger logs another 5 messages after logger has been called another 5 times

--- spec/buffered_logger_2_spec.rb
+++ spec/buffered_logger_2_spec.rb
@@ -14,4 +14,17 @@ describe BufferedLogger do
     expect(actual).to eq (expected)
   end
 
+  it "writes another 5 messages only after log has been called another 5 times, in increments of 5 even if a different number is called" do
+    logger = BufferedLogger.new(File.expand_path("../../lib/new_file.rb", __FILE__))
+
+    13.times do
+      logger.log("hello")
+    end
+
+    actual = "hello #{Time.now}\nhello #{Time.now}\nhello #{Time.now}\nhello #{Time.now}\nhello #{Time.now}\nhello #{Time.now}\nhello #{Time.now}\nhello #{Time.now}\nhello #{Time.now}\nhello #{Time.now}\n"
+    expected = File.open(File.expand_path("../../lib/new_file.rb", __FILE__)).read
+
+    expect(actual).to eq (expected)
+  end
+
 end
